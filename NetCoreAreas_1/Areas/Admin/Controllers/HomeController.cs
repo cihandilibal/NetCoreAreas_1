8feac2652488e31882b122f3fb48a82f5c8e07d1
cihan dilibal <dilibalcihan@gmail.com>
2024-04-30 14:32:00 +0300

    Add project files.

---
+++ NetCoreAreas_1/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreAreas_1.Areas.Admin.Controllers
+{
+    //Area'lar Attribute olarak tanımlanmadan calıstırılamaz.
+    // yani, [Area("Admin")]
+
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
